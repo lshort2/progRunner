7954cd46a7bd52589133a21108c14c68a309e9ac
Lukey <lshort2@mail.csuchico.edu>
2017-02-16 09:31:43 -0800

    update appears to be working exactly as intended

--- progRunner.sh
+++ progRunner.sh
@@ -12,12 +12,16 @@ do
   for j in ./inputFiles/*
   do
     echo "run program with $j as input"
-    #name=${j##*/}   #Remove file extension
     name=$(basename $j)
     name=${name%.*}   #Remove file extension
-    #name="${name}.out"#Append new file extension
-    name=$name"out"#Append new file extension
-    echo " and output it to file named $name"
+    out=".out"
+    str=$(echo "$name$out") #str = name + extension
+
+    outputDir="./outputFiles_$i" #create output directory if none exists
+    mkdir -p $outputDir
+    outputFile="$outputDir/$str"
+
+    $1 <$j >$outputFile #run program
   done
 done
 
